741aecfcc9eac617855e0fc9d1cac5e00f343650
Fernando <tbyuyfcjjll@gmail.com>
2019-05-07 22:17:14 -0500

    Arreglo de la funcion construirAst, ahora funciona adecuadamente.

--- Modoki/Compilador/AnalisisSintactico/Arbol.fs
+++ Modoki/Compilador/AnalisisSintactico/Arbol.fs
@@ -1,16 +1,9 @@
 module Arbol
-open AnalisisLexico
-
-type Token = {
-    valor: string
-    posInicio: datosPosicion
-    posFinal: datosPosicion
-    tipo: tipoToken
-    precedencia: int
-}
+open System
+open TokenMap
 
 type Ast =
-    | Nodo of Token * Ast * Ast
+    | Nodo of TokenMap.Token * Ast * Ast
     | Hoja
 
 let rec preorden arbol =
@@ -25,31 +18,48 @@ let rec inorden arbol =
 
 type Asoc = L | R
 
-let obtPrecedencia token =
-    if token.tipo = tipoToken.Operador then
-        match token.valor with
-        | "<|" | "|>" -> 1
-        | "+" -> 2
-        | "-" -> 3
-        | "*" | "/" | "%" | "%%" -> 4
-        | "**" -> 5
-        | "f" -> 6
-        | "==" | "!=" | "!" | "€" -> 7
-        | "." | "?" | "?:" | "?." -> 8
-        | _ -> 0
-    else
-        0
 
 let rec insertarAAST ast token =
     match ast with
     | Hoja -> Nodo (token, Hoja, Hoja)
+    | Nodo (t, Hoja, Hoja) ->
+        if token.precedencia > t.precedencia && t.precedencia = 0 then
+            Nodo (token, ast, Hoja)
+        else
+            printfn "Token problematico:\n %A \n" token
+            printfn "pr token Nuevo: %i -> pr token Actual: %i " token.precedencia t.precedencia
+            raise (Exception "Error. No se pueden poner 2 valores juntos v:<")
+    | Nodo (t, i, Hoja) ->
+        if token.precedencia = 0 then
+            Nodo (t, i, Nodo (token, Hoja, Hoja))
+        else
+            printfn "Token problematico:\n %A \n" token
+            raise (Exception "Error. No se pueden poner 2 operadores juntos.")
     | Nodo (t, i, d) ->
-        if t.precedencia > token.precedencia && t.precedencia = 0 then
+        
+        if token.precedencia <= t.precedencia && token.precedencia <> 0 then
+            Nodo (token, ast, Hoja)
+        else if token.precedencia <= t.precedencia && token.precedencia = 0 then
+            Nodo (t, i, insertarAAST d token)
+        else
+            Nodo (t, i, insertarAAST d token)
+    
+        (* Codigo malogrado :c
+        if t.precedencia < token.precedencia && t.precedencia = 0 then
             Nodo (token, ast, Hoja)
         else if t.precedencia > token.precedencia && t.precedencia <> 0 && token.precedencia <> 0 then
             Nodo (token, ast, Hoja)
         else match d with
              | Hoja -> Nodo (t, i, Nodo (token, Hoja, Hoja))
-             | Nodo (t', i', d') -> Nodo (t', i', insertarAAST d token)
+             | Nodo (t', i', _) -> Nodo (t', i', insertarAAST d token)
+        *)
 
+let construirAst tokens =
+    let rec construirAst' ast tokens =
+        match tokens with
+        | [] -> ast
+        | x :: xs -> 
+             let nuevoAst = insertarAAST ast x
+             construirAst' nuevoAst xs
 
+    construirAst' Hoja tokens
