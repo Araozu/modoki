faaeffd6b1f9a1de381ae686c6658a3792e79bf8
Fernando <tbyuyfcjjll@gmail.com>
2019-05-13 21:48:35 -0500

    Agregado soporte para Aplicacion de funciones e identificadores.

--- Modoki/Compilador/TablaSimbolos/Tipos.fs
+++ Modoki/Compilador/TablaSimbolos/Tipos.fs
@@ -1,6 +1,8 @@
 module Tipos
 
 type Tipo =
+    | Identificador
+    | FunAppl
     | Tipo of string
     | Lista of Tipo
     | Funcion of Tipo * Tipo
@@ -10,11 +12,11 @@ let vacio = Tipo "vacio"
 
 let rec obtSignature tipo =
     match tipo with
+    | FunAppl -> "funAppl"
+    | Identificador -> "identificador"
     | Tipo nombre -> nombre
     | Lista tipo -> "[" + obtSignature tipo + "]"
     | Funcion (t1, t2) ->
         match t1 with
         | Funcion _ -> "(" + obtSignature t1 + ") -> " + obtSignature t2
         | _ -> obtSignature t1 + " -> " + obtSignature t2
-
-
